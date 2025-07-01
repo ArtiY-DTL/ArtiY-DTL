384baf2cdf7e8e6e187acd7f8b4a5ec1a0594bf2
ArtiY-DTL <arti.y@droptechnolab.com>
2025-07-01 14:21:39 +0530

    pending table

---
+++ table/table.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <title>All Table</title>
+    <link rel="icon" type="image/x-icon" href="https://artiy-dtl.github.io/profile/upload/table.jpg" />
+    
+</html>
+<body>
+    <div class="container">
+
+    </div>
+
+</body>
+</html>
